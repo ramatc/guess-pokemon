17b72e9e3e301507e2cfca322e60b8c241d09c7d
ramatc <rtanquiascornejo@gmail.com>
2023-03-07 18:48:05 -0300

    Add loader

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const Loader = () => {
+  return (
+    <div className='loader'>
+        <h3>Cargando...</h3>
+        <div className='ball'></div>
+    </div>
+  )
+}
+
+export default Loader;
